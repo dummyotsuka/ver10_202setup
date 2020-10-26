520ae8dbadd056fd2fd63d5b5c2ab1b314f429f8
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-10-26 18:56:36 +0900

    Update 202prepare_ConoHa_Cent72_Server.sh

--- 202prepare_ConoHa_Cent72_Server.sh
+++ 202prepare_ConoHa_Cent72_Server.sh
@@ -22,6 +22,7 @@ yum -y install dump
 rm -f /etc/systemd/system/default.target
 ln -s /usr/lib/systemd/system/graphical.target /etc/systemd/system/default.target
 yum -y install mkisofs
+yum -y install epel-release
 yum -y install named
 yum -y install httpd
 
