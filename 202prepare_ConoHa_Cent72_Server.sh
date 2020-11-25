1ab87739a9a47145190e61bf3dedc75302980580
dummyotsuka <64056576+dummyotsuka@users.noreply.github.com>
2020-11-25 20:23:19 +0900

    Update 202prepare_ConoHa_Cent72_Server.sh

--- 202prepare_ConoHa_Cent72_Server.sh
+++ 202prepare_ConoHa_Cent72_Server.sh
@@ -72,6 +72,8 @@ wget https://raw.githubusercontent.com/dummyotsuka/ver10_202setup/main/change-do
 cd ../
 ldapadd -Y EXTERNAL -H ldapi:// -f ./ldap_work/add_rootPw.ldif
 ldapmodify -x -D cn=config -w ngn-sf -f ./ldap_work/change-domain.ldif
-
+ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/cosine.ldif
+ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/inetorgperson.ldif
+ldapadd -Y EXTERNAL -H ldapi:// -f /etc/openldap/schema/nis.ldif 
 
 shutdown -h now
